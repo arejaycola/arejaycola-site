06ebf7e93c3d3d5db287bdcf82d43e56010db0a1
Rob Mastronicola <arejaycola@gmail.com>
2021-01-17 22:31:49 -0500

    Trying to get heroku working

--- server/server.js
+++ server/server.js
@@ -4,8 +4,12 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
+app.use(express.static(path.join(__dirname, '../client/build')));
+
 
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
@@ -16,6 +20,10 @@ app.post('/api/world', (req, res) => {
   res.send(
     `I received your POST request. This is what you sent me: ${req.body.post}`,
   );
+});
+
+app.get('*', async function (req, res) {
+	// res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
